1ecbf276f73cd5fadca3df4ae5196e58a38cf857
Siterizer <siterizer@gmail.com>
2019-12-29 14:53:10 +0100

    Website is now ready to work with php, and fontello is deleted for a moment

---
+++ Views/SecurityController/index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE_HTML>
+
+<html lang="pl">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<title>Najpiękniejsze Pizze Świata</title>
+		<meta name="description" content="Serwis zrzeszający wszystkich fanatyków pięknych pizz na świecie" />
+		<meta name="keywords" content="pizza, pizzy, zdjęcia, obrazki, piękne, piekne" />
+		
+		<link href="../Public/css/style.css" rel="stylesheet" type="text/css" />
+	<!--	<link href="../Public/css/fontello.css" rel="stylesheet" type="text/css" /> -->
+	</head>
+	<body>
+	
+		<div id="container">
+			<nav>
+				<div id="navbar">
+					<br><br>
+					<div class="navigation">
+						<h3> KATEGORIE </h3>
+					</div>
+					<div class="navigation">
+						PIZZE WŁOSKIE
+					</div>
+					<div class="navigation">
+						PIZZE GRUBE
+					</div>
+					<div class="navigation">
+						PIZZE CIEŃKIE
+					</div>
+					<div class="navigation">
+						PIZZE SEROWE
+					</div>
+					<div class="navigation">
+						PIZZE ŚWIATA
+					</div>
+					<br>
+					<div class="navigation">
+						PANEL UŻYTKOWNIKA
+					</div>
+					<div class="navigation">
+						REJESTRACJA
+					</div>
+					<div class="navigation">
+						LOGIN
+					</div>
+					<div class="navigation">
+						RESET HASŁA
+					</div>
+					<div class="navigation">
+						ZMIANA HASŁA
+					</div>
+					
+				<!--	<div id="icons">
+					
+						<div class="icon">
+							<i class="icon-instagram"></i>
+						</div>
+						<div class="icon">
+							<i class="icon-twitter"></i>
+						</div>
+						<div class="icon">
+							<i class="icon-facebook"></i>
+						</div>
+						
+					</div>-->
+				</div>
+			</nav>
+			<div id="wrapper">
+				
+				<div id="logoBar">
+				
+					<div id="logoContainer">
+						<img src="../Public/img/logo.png" id="logoImage">
+						<header>
+							<div id="logoSubtitle">
+								Najpiękniejsze Pizze Świata
+							</div>
+						</header>
+					</div>
+				
+				</div>
+				
+				<div id="posts">
+					<section>
+						<div class="post">
+							<img src="../Public/img/1.jpg" class="PostImage">
+							<div class="postDescription">
+								<article>
+									<h3> Top dzisiaj </h3>
+									Jakis napis
+								</article>
+								<div class="postBottom">
+									<i class="icon-heart"></i>
+									609
+									<span style="display:inline-block; width: 10;"></span>
+									<i class="icon-minus-squared"></i>
+									120
+									<span style="display:inline-block; width: 55;"></span>
+									<span style="font-weight: 700">SHARE</span>
+								</div>
+							</div>
+						</div>
+						
+						<div class="post">
+							<img src="../Public/img/2.jpg" class="PostImage">
+							<div class="postDescription">
+								<article>
+									<h3> Top tygodnia </h3>
+									Jakis napis
+								</article>
+								<div class="postBottom">
+									<i class="icon-heart"></i>
+									609
+									<span style="display:inline-block; width: 10;"></span>
+									<i class="icon-minus-squared"></i>
+									120
+									<span style="display:inline-block; width: 55;"></span>
+									<span style="font-weight: 700">SHARE</span>
+								</div>
+							</div>
+						</div>
+						
+						<div class="post">
+							<img src="../Public/img/1.jpg" class="PostImage">
+							<div class="postDescription">
+								<article>
+									<h3> Top miesiąca </h3>
+									Jakis napis
+								</article>
+								<div class="postBottom">
+									<i class="icon-heart"></i>
+									609
+									<span style="display:inline-block; width: 10;"></span>
+									<i class="icon-minus-squared"></i>
+									120
+									<span style="display:inline-block; width: 55;"></span>
+									<span style="font-weight: 700">SHARE</span>
+								</div>
+							</div>
+						</div>
+						
+						<div class="post">
+							<img src="../Public/img/2.jpg" class="PostImage">
+							<div class="postDescription">
+								<article>
+									<h3> Top roku </h3>
+									Jakis napis
+								</article>
+								<div class="postBottom">
+									<i class="icon-heart"></i>
+									609
+									<span style="display:inline-block; width: 10;"></span>
+									<i class="icon-minus-squared"></i>
+									120
+									<span style="display:inline-block; width: 55;"></span>
+									<span style="font-weight: 700">SHARE</span>
+								</div>
+							</div>
+						</div>
+						
+						<div class="post">
+							<img src="../Public/img/1.jpg" class="PostImage">
+							<div class="postDescription">
+								<article>
+									<h3> Top stulecia </h3>
+									Jakis napis
+								</article>
+								<div class="postBottom">
+									<i class="icon-heart"></i>
+									609
+									<span style="display:inline-block; width: 10;"></span>
+									<i class="icon-minus-squared"></i>
+									120
+									<span style="display:inline-block; width: 55;"></span>
+									<span style="font-weight: 700">SHARE</span>
+								</div>
+							</div>
+						</div>
+					<section>
+				</div>
+			</div>
+			<footer>
+				<div id="footer">
+					<div id="footerContainer">
+						<div id="footerContactUs">
+							<h3> Contact us: </h3>
+							+123 456 789 <br><br>
+							NajpiekniejszePizzeSwiata@gmail.com
+						</div>
+						<div id="footerCustomerRevice">
+							<h3> Customer Revice: </h3>
+							Contact us <br><br>
+							Ordering & Payment <br><br>
+							Shipping <br><br>
+							Returns <br><br>
+							FAQ
+						</div>
+						<div id="footerInformation">
+							<h3> Information: </h3>
+							About Najpiękniejsze Pizee Świata <br><br>
+							Work With Us <br><br>
+							Privacy Policy <br><br>
+							Terms & Conditions <br><br>
+							Press Enquiries
+						</div>
+						<div id="footerSubscribe">
+						<br></br>
+							<h3> Subscribe to Najpiekniejsze Pizze Świata: </h3>
+							<br></br>
+							<div id="emailText">
+								<input type="text" name="email" value="Email Adress"><br>
+							</div>
+							<div id="emailSubmit">
+								<input type="submit" name="email_submit" value="Subscribe"><br>
+							</div>
+							</br>
+						</div>
+					
+					</div>
+				</div>
+			</footer>
+	
+		
+		</div>
+	</body>
+</html>
