d4b14990cf62237cbe027766fd315cd45cd2591e
siterizer <siterizer@gmail.com>
2020-01-01 20:56:22 +0100

    Footer is now built on lists, not dives

--- Views/Common/footer.php
+++ Views/Common/footer.php
@@ -1,40 +1,32 @@
 <footer>
-				<div id="footer">
-					<div id="footerContainer">
-						<div id="footerContactUs">
-							<h3> Contact us: </h3>
-							+123 456 789 <br><br>
-							NajpiekniejszePizzeSwiata@gmail.com
-						</div>
-						<div id="footerCustomerRevice">
-							<h3> Customer Revice: </h3>
-							Contact us <br><br>
-							Ordering & Payment <br><br>
-							Shipping <br><br>
-							Returns <br><br>
-							FAQ
-						</div>
-						<div id="footerInformation">
-							<h3> Information: </h3>
-							About Najpiękniejsze Pizee Świata <br><br>
-							Work With Us <br><br>
-							Privacy Policy <br><br>
-							Terms & Conditions <br><br>
-							Press Enquiries
-						</div>
-						<div id="footerSubscribe">
-						<br></br>
-							<h3> Subscribe to Najpiekniejsze Pizze Świata: </h3>
-							<br></br>
-							<div id="footerEmailTextWrapper">
-								<input type="text" name="email" value="Email Adress" id="footerEmailText"><br>
-							</div>
-							<div id="footerEmailSubmitWrapper">
-								<input type="submit" name="email_submit" value="Subscribe" id="footerEmailSubmit"><br>
-							</div>
-							</br>
-						</div>
+	<div id="footer">
+		<ul class = "footerInformation">
+			<li> <h3> Contact us: </h3></li>
+			<li> +123 456 789 </li>
+			<li> NajpiekniejszePizzeSwiata@gmail.com </li>
+		</ul>
+		<ul class = "footerInformation">
+			<li> <h3> Customer Revice: </h3></li>
+			<li> Contact us </li>
+			<li> Ordering & Payment </li>
+			<li> Shipping </li>
+			<li> Returns </li>
+			<li> FAQ </li>
+		</ul>
+		<ul class = "footerInformation">
+			<li> <h3> Information: </h3></li>
+			<li> About Najpiękniejsze Pizee Świata </li>
+			<li> Work With Us </li>
+			<li> Privacy Policy </li>
+			<li> Terms & Conditions </li>
+			<li> Press Enquiries </li>
+		</ul>
+		<div id="footerSubscribe">
+			<h3> Subscribe to Najpiekniejsze Pizze Świata: </h3>
+				<input type="text" name="email" value="Email Adress" id="footerEmailText">
+				<input type="submit" name="email_submit" value="Subscribe" id="footerEmailSubmit">
+			</br>
+		</div>
 					
-					</div>
-				</div>
-			</footer>
+	</div>
+	</footer>
